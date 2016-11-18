ef1dfa7594623797c76a311860034eb9d24bf8d4
patlenew <pat.lenouveau@gmail.com>
2016-11-18 10:51:59 -0500

    added basic test stuff, script mouvement, collision tests

---
+++ metroidVapeur/Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    float speed = 5.0f;
+    private string findName = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") * 5, 0);
+
+
+        if (transform.position.y >= 2.5f)
+        {
+            transform.position = new Vector3(transform.position.x, 2.5f, 0);
+        }
+        else
+        {
+            transform.position += move * speed * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(Main.doorArrEnterRef.Contains(col.gameObject))
+        {
+            findName = col.gameObject.ToString();
+            int index = Main.doorArrEnterRef.IndexOf(col.gameObject);
+            transform.position = new Vector3(Main.doorArrExitRef[index].transform.position.x + 1.5f, Main.doorArrExitRef[index].transform.position.y + 1.5f, 0);
+        }
+        else if(Main.doorArrExitRef.Contains(col.gameObject))
+        {
+            findName = col.gameObject.ToString();
+            int index = Main.doorArrExitRef.IndexOf(col.gameObject);
+            transform.position = new Vector3(Main.doorArrEnterRef[index].transform.position.x - 1.5f, Main.doorArrEnterRef[index].transform.position.y + 1.5f, 0);
+        }
+    }
+
+    private bool isName(string name)
+    {
+        return (name == findName);  
+    }
+}
--- metroidVapeur/Assets/Scripts/Main.cs
+++ metroidVapeur/Assets/Scripts/Main.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
 public class Main : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public GameObject playerRef;
+    public Transform playerT;
+    
+    
+    public  List<GameObject> doorArrEnter = new List<GameObject>();
+    public  List<GameObject> doorArrExit = new List<GameObject>();
+
+    public static List<GameObject> doorArrEnterRef = new List<GameObject>();
+    public static List<GameObject> doorArrExitRef = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        doorArrEnterRef = doorArrEnter;
+        doorArrExitRef = doorArrExit;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.position = new Vector3(playerT.transform.position.x, playerT.transform.position.y, -10);
 	}
+
+   
 }
